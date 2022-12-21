aca3096eddbc893ec12e4feaf99511d050f3333d
subhasishdas024 <90309119+subhasishdas024@users.noreply.github.com>
2022-12-21 14:48:25 +0530

    Delete app directory

--- app/src/main/java/io/groupproject/renovateit/Common/Common.java
+++
@@ -1,7 +0,0 @@
-package io.groupproject.renovateit.Common;
-
-import io.groupproject.renovateit.Model.User;
-
-public class Common {
-    public static User currentUser;
-}
--- app/src/main/java/io/groupproject/renovateit/Interface/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package io.groupproject.renovateit.Interface;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onClick(View view,int pos,boolean isLongClick);
-}
